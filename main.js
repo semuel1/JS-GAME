e6fd24d4572a5a1513872f9df41f0656790122e8
semuel1 <semuelst1@gmail.com>
2021-02-09 13:38:54 -0800

    Js-Game

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log('hi')
+//Start game
+
+
+//Score - when mouse-enter - score display
+let myScore = 0
+
+//lost life - when div gets to the bottom
+
+//Game over
+
+
+//Restart
+
+//slice 
+let sprite1 =  document.querySelector("#Sprite1")
+sprite1.addEventListener('mouseenter', function() {
+    myScore++;
+    let addScore = document.querySelector('#score')
+    addScore.innerText = myScore
+    sprite1.classList.add('disappear')
+})
+
+
+
