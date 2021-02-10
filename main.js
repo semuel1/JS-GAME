7f2f195c4a99b46cddf5eb201991f6be31c1ec6f
semuel1 <semuelst1@gmail.com>
2021-02-10 10:19:41 -0800

    Updates

--- main.js
+++ main.js
@@ -20,5 +20,19 @@ sprite1.addEventListener('mouseenter', function() {
     sprite1.classList.add('disappear')
 })
 
+let sprite2 =  document.querySelector("#Sprite2")
+sprite2.addEventListener('mouseenter', function() {
+    myScore++;
+    let addScore = document.querySelector('#score')
+    addScore.innerText = myScore
+    sprite2.classList.add('disappear')
+})
 
+let sprite3 =  document.querySelector("#Sprite3")
+sprite3.addEventListener('mouseenter', function() {
+    myScore++;
+    let addScore = document.querySelector('#score')
+    addScore.innerText = myScore
+    sprite3.classList.add('disappear')
+})
 
