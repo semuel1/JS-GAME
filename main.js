3797e8d91770cd01e4f7a92b55e6f42880ba10d0
semuel1 <semuelst1@gmail.com>
2021-02-12 14:31:10 -0800

    update

--- main.js
+++ main.js
@@ -28,14 +28,50 @@ function start() {
     sprite1.append(image1)
     document.querySelector('body').append(sprite1)
     sprite1.addEventListener('mouseenter', handleMouseEvent)
-
+    
+    const sprite2 = document.createElement('div')
+    sprite2.id = "Sprite2"
+    sprite2.classList.add('GamePieces')
+    const image2 = document.createElement('img')
+    image2.src = './images/Anna-mae.jpg'
+    sprite2.append(image2)
+    document.querySelector('body').append(sprite2)
+    sprite2.addEventListener('mouseenter', handleMouseEvent)
+    
+    const sprite3 = document.createElement('div')
+    sprite3.id = "Sprite3"
+    sprite3.classList.add('GamePieces')
+    const image3 = document.createElement('img')
+    image3.src = './images/Marry-o.png'
+    sprite3.append(image3)
+    document.querySelector('body').append(sprite3)
+    sprite3.addEventListener('mouseenter', handleMouseEvent)
+    
+    const sprite4 = document.createElement('div')
+    sprite4.id = "Sprite4"
+    sprite4.classList.add('GamePieces')
+    const image4 = document.createElement('img')
+    image4.src = './images/pngwing.com.png'
+    sprite4.append(image4)
+    document.querySelector('body').append(sprite4)
+    sprite4.addEventListener('mouseenter', handleMouseEvent)
+    
+    const sprite5 = document.createElement('div')
+    sprite5.id = "Sprite5"
+    sprite5.classList.add('GamePieces')
+    const image5 = document.createElement('img')
+    image5.src = './images/Kalari.png'
+    sprite5.append(image5)
+    document.querySelector('body').append(sprite5)
+    sprite5.addEventListener('mouseenter', handleMouseEvent)
+    
 }
 
 
 
 
 function removeTargets() {
-   let gamePieces = document.querySelectorAll('.GamePieces')
+    let gamePieces = document.querySelectorAll('.GamePieces')
     for (let i=0; i < gamePieces.length; i++) {
         gamePieces[i].remove()
     }
@@ -53,14 +89,15 @@ function reset(event){
 function win_condition() {
     if (myScore == 5) {
         return 'You Win!';
-     }
+    }
     
     if (myScore < 5 ) {
         return 'GameOver';
-  }
+    }
 }
 
 
+// *Counter will indicate point system.
 // *When I move my cursor through an object, the object should slice in half, gaining me a single point.
 function handleMouseEvent(event) {
     console.log(event.target)
@@ -71,7 +108,6 @@ function handleMouseEvent(event) {
 // *When I slice the object, a new object will appear for me to slice.
 // *When you miss three objects the game will end.
 // *When you miss an object a couter will indicate that you missed.
-// *Counter will indicate point system.
 
 /* Event Listeners */
 
