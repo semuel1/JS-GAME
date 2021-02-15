ade5919bc64923f295b29d783f147af627794768
semuel1 <semuelst1@gmail.com>
2021-02-15 00:50:58 -0800

    update

--- main.js
+++ main.js
@@ -1,74 +1,85 @@
 /* Constants */
 /* Game Logic Variables and State */
 let myScore = 0
-let misses = 0
+let myLives = 3
+let sprite1Time;
 
 /* DOM References */
 
-let sprite1 =  document.querySelector("#Sprite1")
+let sprite1 =  document.querySelector("#sprite1")
 let sprite2 =  document.querySelector("#Sprite2")
 let sprite3 =  document.querySelector("#Sprite3")
 let sprite4 =  document.querySelector("#Sprite4")
 let sprite5 =  document.querySelector("#Sprite5")
 
-let addScore = document.querySelector('#score')
+let scoreText = document.querySelector('#score')
 let gameOver = document.querySelector('#GameOver')
 let gamePieces = document.querySelectorAll('.GamePieces')
 let startGame = document.querySelector('#Start')
-
+let lifeText = document.querySelector('#lives')
 
 
 /* Functions and Game Logic */
 function start() {
     const sprite1 = document.createElement('div')
-    sprite1.id = "Sprite1"
+    sprite1.id = "sprite1"
     sprite1.classList.add('GamePieces')
     const image1 = document.createElement('img')
     image1.src = './images/Jel-Aye.jpg'
     sprite1.append(image1)
     document.querySelector('body').append(sprite1)
-    sprite1.addEventListener('mouseenter', handleMouseEvent)
+    sprite1.addEventListener('mouseenter', handleSlice)
+    sprite1Time = setTimeout(function(){
+        myLives -= 1;
+        setLives();
+    }, 4000);
+
     
-    const sprite2 = document.createElement('div')
-    sprite2.id = "Sprite2"
-    sprite2.classList.add('GamePieces')
-    const image2 = document.createElement('img')
-    image2.src = './images/Anna-mae.jpg'
-    sprite2.append(image2)
-    document.querySelector('body').append(sprite2)
-    sprite2.addEventListener('mouseenter', handleMouseEvent)
+    // const sprite2 = document.createElement('div')
+    // sprite2.id = "Sprite2"
+    // sprite2.classList.add('GamePieces')
+    // const image2 = document.createElement('img')
+    // image2.src = './images/Anna-mae.jpg'
+    // sprite2.append(image2)
+    // document.querySelector('body').append(sprite2)
+    // sprite2.addEventListener('mouseenter', handleSlice)
     
-    const sprite3 = document.createElement('div')
-    sprite3.id = "Sprite3"
-    sprite3.classList.add('GamePieces')
-    const image3 = document.createElement('img')
-    image3.src = './images/Marry-o.png'
-    sprite3.append(image3)
-    document.querySelector('body').append(sprite3)
-    sprite3.addEventListener('mouseenter', handleMouseEvent)
+    // const sprite3 = document.createElement('div')
+    // sprite3.id = "Sprite3"
+    // sprite3.classList.add('GamePieces')
+    // const image3 = document.createElement('img')
+    // image3.src = './images/Marry-o.png'
+    // sprite3.append(image3)
+    // document.querySelector('body').append(sprite3)
+    // sprite3.addEventListener('mouseenter', handleSlice)
     
-    const sprite4 = document.createElement('div')
-    sprite4.id = "Sprite4"
-    sprite4.classList.add('GamePieces')
-    const image4 = document.createElement('img')
-    image4.src = './images/pngwing.com.png'
-    sprite4.append(image4)
-    document.querySelector('body').append(sprite4)
-    sprite4.addEventListener('mouseenter', handleMouseEvent)
+    // const sprite4 = document.createElement('div')
+    // sprite4.id = "Sprite4"
+    // sprite4.classList.add('GamePieces')
+    // const image4 = document.createElement('img')
+    // image4.src = './images/pngwing.com.png'
+    // sprite4.append(image4)
+    // document.querySelector('body').append(sprite4)
+    // sprite4.addEventListener('mouseenter', handleSlice)
     
-    const sprite5 = document.createElement('div')
-    sprite5.id = "Sprite5"
-    sprite5.classList.add('GamePieces')
-    const image5 = document.createElement('img')
-    image5.src = './images/Kalari.png'
-    sprite5.append(image5)
-    document.querySelector('body').append(sprite5)
-    sprite5.addEventListener('mouseenter', handleMouseEvent)
+    // const sprite5 = document.createElement('div')
+    // sprite5.id = "Sprite5"
+    // sprite5.classList.add('GamePieces')
+    // const image5 = document.createElement('img')
+    // image5.src = './images/Kalari.png'
+    // sprite5.append(image5)
+    // document.querySelector('body').append(sprite5)
+    // sprite5.addEventListener('mouseenter', handleSlice)
     
+    setLives()
 }
 
+function setLives() {
+    lifeText.innerText = myLives
+}
+//
 
-
+//
 
 function removeTargets() {
     let gamePieces = document.querySelectorAll('.GamePieces')
@@ -81,30 +92,32 @@ function removeTargets() {
 // *When I press start game, the game is reset and then the object float on the screen.
 function reset(event){
     myScore = 0
-    misses = 0
     showSprite.classList.toggle('');
 }
 
 // *When you slice 5 objects, game is won.
-function win_condition() {
-    if (myScore == 5) {
-        return 'You Win!';
+function checkWin() {
+    if (myScore === 5) {
+        alert('You Win!');
     }
-    
-    if (myScore < 5 ) {
-        return 'GameOver';
+
+function checkLose()
+    if (myLives === 0 ) {
+        alert ('GameOver');
     }
 }
-
 
 // *Counter will indicate point system.
 // *When I move my cursor through an object, the object should slice in half, gaining me a single point.
-function handleMouseEvent(event) {
-    console.log(event.target)
+function handleSlice(event) {
     myScore++;
-    addScore.innerText = myScore
-    event.target.classList.add('disappear')
+    scoreText.innerText = myScore;
+
+    event.target.classList.add('disappear');
+    console.log(event.target.id + 'Time')
+    clearTimeout(event.target.id + 'Time');
 }
+
 // *When I slice the object, a new object will appear for me to slice.
 // *When you miss three objects the game will end.
 // *When you miss an object a couter will indicate that you missed.
@@ -112,11 +125,11 @@ function handleMouseEvent(event) {
 /* Event Listeners */
 
 // startGame.addEventListener('click', reset)
-// sprite1.addEventListener('mouseenter', handleMouseEvent)
-// sprite2.addEventListener('mouseenter', handleMouseEvent)
-// sprite3.addEventListener('mouseenter', handleMouseEvent)
-// sprite4.addEventListener('mouseenter', handleMouseEvent)
-// sprite5.addEventListener('mouseenter', handleMouseEvent)
+// sprite1.addEventListener('mouseenter', handleSlice)
+// sprite2.addEventListener('mouseenter', handleSlice)
+// sprite3.addEventListener('mouseenter', handleSlice)
+// sprite4.addEventListener('mouseenter', handleSlice)
+// sprite5.addEventListener('mouseenter', handleSlice)
 startGame.addEventListener('click', start)
 
 for(gamePiece of gamePieces) {
